3eab11d9b61eac07a813bc6eb6b4ad7d5d51e4c2
klvnbett <kelvin.bett@student.moringaschool.com>
2022-05-09 16:44:04 +0300

    alpha commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const primaryDispaly = document.querySelector('.primary-display');
+const buttons = document.querySelector('.buttons-container');
+
+buttons.addEventListener('click', (event) => {
+    // primaryDispaly.innerHTML += event.target.innerHTML;
+    if (event.target.classList)
+});
